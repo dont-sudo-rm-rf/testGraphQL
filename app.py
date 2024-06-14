0d5ad5e068b3f15c54ae1b09cb88e4091e68d6d6
David Halmy <David.Halmy@us.kbr.com>
2024-06-14 09:07:13 -0400

    added comments

--- app.py
+++ app.py
@@ -32,37 +32,42 @@ books = [
 def generate_id():
     return len(books) + 1
 
-# GraphQL schema
+# GraphQL schema for a book
 class Book(graphene.ObjectType):
-    id = graphene.String(description = " number of the book", required = True)
+    id = graphene.String(description="Number of the book", required=True)
     name = graphene.String(description="Name of the book", required=True)
     author = graphene.String(description="Author of the book", required=True)
     status = graphene.String(description="Status of the book", required=True)
 
+# Defines the available Queries
 class Query(graphene.ObjectType):
+    # What you wish to query
     all_books = graphene.List(Book)
 
-    
-
+    # Resolver function (required. To my understand it needs to follow structure 'resolve'_'name')
     def resolve_all_books(self, info):
         return books
 
     book_by_id = graphene.Field(Book, book_id=graphene.Int(required=True))
-    
+
     def resolve_book_by_id(self, info, book_id):
         for book in books:
             if book['id'] == str(book_id):
                 return book
         return None
 
+# Define the CreateBook mutation
 class CreateBook(graphene.Mutation):
     class Arguments:
+        # Input arguments for the mutation
         name = graphene.String(required=True)
         author = graphene.String(required=True)
         status = graphene.String(required=True)
 
+    # The response of the mutation
     book = graphene.Field(Book)
 
+    # The mutate method handles the mutation logic
     def mutate(self, info, name, author, status):
         new_book = {
             'id': generate_id(),
@@ -73,9 +78,11 @@ class CreateBook(graphene.Mutation):
         books.append(new_book)
         return CreateBook(book=new_book)
 
+# Mutation class that includes the CreateBook mutation
 class Mutation(graphene.ObjectType):
     create_book = CreateBook.Field()
 
+# Include both Query and Mutation in the schema
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 app.add_url_rule(
@@ -88,17 +95,19 @@ app.add_url_rule(
 )
 
 
-# GraphQL endpoint
-@app.route('/graphql', methods=['GET', 'POST'])
-def graphql_endpoint():
-    if request.method == 'GET':
-        # Handle GET request to fetch schema
-        return jsonify({'schema': schema.to_dict()})
-    elif request.method == 'POST':
-        # Handle POST request for executing GraphQL queries
-        data = request.get_json()
-        result = schema.execute(data.get('query'))
-        return jsonify(result.data)
+# Alternative to app.add_url_rule (app.add_url_rule works just as well) 
+
+# # GraphQL endpoint
+# @app.route('/graphql', methods=['GET', 'POST'])
+# def graphql_endpoint():
+#     if request.method == 'GET':
+#         # Handle GET request to fetch schema
+#         return jsonify({'schema': schema.to_dict()})
+#     elif request.method == 'POST':
+#         # Handle POST request for executing GraphQL queries
+#         data = request.get_json()
+#         result = schema.execute(data.get('query'))
+#         return jsonify(result.data)
 
 
 if __name__ == '__main__':
